a414fb0beec92f4e84da76a6d63ce479ad9fb2d2
Armadillo <ajknapp2015@gmail.com>
2020-01-24 23:49:14 -0800

    Created a hide all function which serves to refresh the page and allow for adding of any page chosen without the others getting in the way.

--- script.js
+++ script.js
@@ -19,30 +19,71 @@ let highscorePage = document.getElementById('highscore-page');
 let goBack = document.getElementById('go-back');
 let clearHighscores = document.getElementById('clear-highscores');
 
-startButton.addEventListener("click", startQuiz);
-//submitInitial.addEventListener("click", openHighscorePage);
-//goBack.addEventListener("click", openStartPage);
+let pageArray = [startPage, questionPage, intitialEnter, highscorePage];
 
 
-let quizTime = 10;
-let secondsLeft = quizTime;
+    viewScores.addEventListener("click", openHighscorePage);
+    startButton.addEventListener("click", startQuiz);
+    submitInitial.addEventListener("click", openHighscorePage);
+    goBack.addEventListener("click", openStartPage);
+
+//Takes a function of all DOM pointers as an array and adds clase hide to each
+function hideAll() {
+    for (let i=0; i<pageArray.length; i++) {
+        if (!pageArray[i].classList.contains('hide')) {
+            pageArray[i].classList.add('hide');
+        }
+    }
+}
+
+let isQuizzing = false; //Will be True when questions are on the screen
+let quizTime = 5; //quizTime is how may seconds the test will last.
+let secondsLeft; //current number of seconds left on timer
+timer.textContent = "Time: " + quizTime; //Prints initial timer on screen
+
 function startQuiz() {
-    //redirects page to quiz.html
-    window.location.href = 'quiz.html'
+
+    secondsLeft = quizTime;
+    isQuizzing = true;
+
+    hideAll();
+    questionPage.classList.remove('hide');
     
     let timerInterval = setInterval(function() {
-        timer.textContent = "Time: " + secondsLeft;
+        
         secondsLeft--;
+        timer.textContent = "Time: " + secondsLeft;
 
-        if (secondsLeft === 0) {
+        if (secondsLeft === 0 || !isQuizzing) {
             clearInterval(timerInterval);
-            
             secondsLeft = quizTime;
+            timer.textContent = "Time: " + secondsLeft;
+            
+            if (isQuizzing) {
+                openInitialsPage();
+            }
         }
         
     }, 1000);
+
+    return "";
 }
 
+function openInitialsPage() {
+    hideAll();
+    intitialEnter.classList.remove('hide');
+}
+
+function openHighscorePage() {
+    isQuizzing = false;
+    hideAll();
+    highscorePage.classList.remove('hide');
+}
+
+function openStartPage() {
+    hideAll();
+    startPage.classList.remove('hide');
+}
 
 
     
